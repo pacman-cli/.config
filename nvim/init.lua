a09f5bb58bc4b2be6ccff4a94f1698bdc0b707be
spowerrr <sk1969363@gmail.com>
2025-03-12 00:21:55 +0600

    updated code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 
 -- For init.lua
+vim.opt.guifont = "Dank Mono:h14"
 vim.opt.swapfile = false
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -20,3 +21,29 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- Disable providers you don't actively use
+vim.g.loaded_perl_provider = 0  -- Disable Perl provider
+vim.g.loaded_ruby_provider = 0  -- Disable Ruby provider if you don't use it
+
+-- Set up formatters for different filetypes
+vim.cmd([[
+  autocmd FileType json setlocal formatprg=prettier\ --parser\ json
+  autocmd FileType html setlocal formatprg=prettier\ --parser\ html
+]])
+
+-- Fix dashboard conflict
+vim.g.dashboard_disable_at_vimenter = 1
+
+-- Configure treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "query",
+    -- Add other languages you need
+  },
+  highlight = {
+    enable = true,
+  },
+}
